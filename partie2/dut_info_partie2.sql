18f5102d146d5dd726f0bddd089e7aab0a9659ab
EliottGULS <eliott.guillossou@gmail.com>
2022-05-13 15:18:46 +0200

    candidat wbimport
    we will need a temp table to import no_candidat matching our id_individu we have created

--- partie2/dut_info_partie2.sql
+++ partie2/dut_info_partie2.sql
@@ -1,33 +1,36 @@
-drop schema if exists partie1 cascade;
-create schema partie1;
-set schema 'partie1';
+drop schema if exists partie2 cascade;
+create schema partie2;
+set schema 'partie2';
 
 
 /**********************
 *       CANDIDAT      *
 **********************/
+drop table if exists _candidat;
 CREATE TABLE _candidat(
       no_candidat         INT,
       id_individu         INT,
       classement          VARCHAR(5),
-      boursier_lycee      VARCHAR(20) not null,
-      profil_candidat     VARCHAR(20) not null,
-      etablissement       VARCHAR(40) not null,
-      dept_etablissement  VARCHAR(2) not null,
-      ville_etablissement VARCHAR(30) not null,
-      niveau_etude        VARCHAR(20) not null,
-      type_formation_prec VARCHAR(15) not null,
-      serie_prec          VARCHAR(1),
-      dominante_prec      VARCHAR(20),
-      specialite_prec     VARCHAR(30),
-      LV1                 VARCHAR(20) not null,
-      LV2                 VARCHAR(25) not null,
+      boursier_lycee      VARCHAR(100),--30
+      profil_candidat     VARCHAR(100) not null,--30
+      etablissement       VARCHAR(50),--30
+      dept_etablissement  VARCHAR(5), -- 2
+      ville_etablissement VARCHAR(100), --50
+      niveau_etude        VARCHAR(50), --30
+      type_formation_prec VARCHAR(50), --10 de base
+      serie_prec          VARCHAR(100), --2 de base
+      dominante_prec      VARCHAR(100),--30
+      specialite_prec     VARCHAR(100), --50
+      LV1                 VARCHAR(40),--30
+      LV2                 VARCHAR(25),
        constraint PK_CANDIDAT primary key (no_candidat)
+       FOREIGN KEY
       );
       
 /**********************
 *       INDIVIDU      *
 **********************/
+drop table if exists _individu Cascade;
 CREATE TABLE _individu(
       id_individu         INT,
       nom                 VARCHAR(30) not null,
@@ -38,6 +41,7 @@ CREATE TABLE _individu(
       ville               VARCHAR(30), --null
       nationalite         VARCHAR(20) not null,
       INE                 VARCHAR(11) not null,
+      UNIQUE(INE),
       CONSTRAINT PK_INDIVIDU PRIMARY KEY(id_individu));
     
 
@@ -147,19 +151,29 @@ ALTER TABLE _etudiant
       ADD CONSTRAINT fk_etudiant_incription FOREIGN KEY (id_individu)
           REFERENCES _individu(id_individu);
 
-WbImport -file= C:\Users\eliot\OneDrive\Bureau\IUT\SAE\S2\SAE2.04\partie2\data\v_candidatures.csv
+
+
+         
+
+WbImport -file=/home/etuinfo/eguillossou/Téléchargements/v_candidatures.csv
+         -header=true
+         -delimiter=';'
+         -table=_candidat
+         -schema=partie2
+         -filecolumns=$wb_skip$, no_candidat, classement, $wb_skip$, $wb_skip$, 
+          $wb_skip$, $wb_skip$, $wb_skip$, $wb_skip$, $wb_skip$, $wb_skip$,
+          boursier, profil_candidat, $wb_skip$, $wb_skip$, libelle_etablissement,
+          ville_etablissement, dept_etablissement, $wb_skip$, niveau_etude, 
+          type_formation, serie_prec, dominante_prec, specialite_prec, lv1, lv2;
+         
+WbImport -file= /home/etuinfo/eguillossou/Téléchargements/v_candidatures.csv
          -header=true
          -delimiter=';'
          -table=_individu
          -schema=partie1
          -fileColumns=$wb_skip$, id_individu, $wb_skip$,  nom, prenom, sexe, date_naissance, nationalite, code_postal, ville, $wb_skip$, $wb_skip$, $wb_skip$, INE;
 
-WbImport -file=C:\Users\eliot\OneDrive\Bureau\IUT\SAE\S2\SAE2.04\partie2\data\ppn.csv
-         -header=true
-         -delimiter=';'
-         -table=_module
-         -schema=partie1
-         -filecolumns=id_module,ue,libelle_module
-         
+
 
 ;
+
