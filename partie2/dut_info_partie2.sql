3b56c5f61c26bc6a9f92aa7b50c6eb9840d26649
Axel Michelo <94619562+aspenne@users.noreply.github.com>
2022-05-23 08:52:46 +0200

    Update dut_info_partie2.sql

--- partie2/dut_info_partie2.sql
+++ partie2/dut_info_partie2.sql
@@ -5,32 +5,32 @@ set schema 'partie2';
 CREATE TABLE _candidat(
       no_candidat         INT,
       id_individu         INT,  
-      classement          VARCHAR(5),
-      boursier_lycee      VARCHAR(20) not null, 
-      profil_candidat     VARCHAR(20) not null,
-      etablissement       VARCHAR(40),
-      dept_etablissement  VARCHAR(2),
-      ville_etablissement VARCHAR(30),
-      niveau_etude        VARCHAR(20),
-      type_formation_prec VARCHAR(15),
-      serie_prec          VARCHAR(100), 
-      dominante_prec      VARCHAR(20),
-      specialite_prec     VARCHAR(30),
-      LV1                 VARCHAR(20),
-      LV2                 VARCHAR(25),
+      classement          VARCHAR(50),
+      boursier_lycee      VARCHAR(200), 
+      profil_candidat     VARCHAR(200),
+      etablissement       VARCHAR(400),
+      dept_etablissement  VARCHAR(200),
+      ville_etablissement VARCHAR(300),
+      niveau_etude        VARCHAR(200),
+      type_formation_prec VARCHAR(150),
+      serie_prec          VARCHAR(1000), 
+      dominante_prec      VARCHAR(200),
+      specialite_prec     VARCHAR(300),
+      LV1                 VARCHAR(200),
+      LV2                 VARCHAR(250),
        constraint PK_CANDIDAT primary key (no_candidat)
       );
       
 CREATE TABLE _individu(
       id_individu         SERIAL,
-      nom                 VARCHAR(30) not null,
-      prenom              VARCHAR(20),
-      date_naissance      DATE not null,
-      code_postal         VARCHAR(30) not null,
-      ville               VARCHAR(50) not null,
-      sexe                VARCHAR(8) not null,
-      nationalite         VARCHAR(20) not null,
-      INE                 VARCHAR(11) not null,
+      nom                 VARCHAR(300) ,
+      prenom              VARCHAR(200),
+      date_naissance      DATE,
+      code_postal         VARCHAR(300),
+      ville               VARCHAR(500),
+      sexe                VARCHAR(80),
+      nationalite         VARCHAR(200),
+      INE                 VARCHAR(110),
       CONSTRAINT PK_INDIVIDU PRIMARY KEY(id_individu)
 );
     
@@ -49,10 +49,10 @@ CREATE TABLE _etudiant(
       CONSTRAINT PK_ETUDIANT PRIMARY KEY(code_nip));
       
 CREATE TABLE _semestre(
-      id_semestre                 SERIAL, 
+      id_semestre                 SERIAL PRIMARY KEY, 
       num_semestre                CHAR(5) not null,
       annee_univ                  CHAR(9) not null,
-      CONSTRAINT PK_SEMESTRE PRIMARY KEY(annee_univ, num_semestre));
+      UNIQUE(annee_univ, num_semestre));
       
 CREATE TABLE _inscription(
       code_nip                    VARCHAR(8),
@@ -69,11 +69,11 @@ CREATE TABLE _module(
       
 
 CREATE TABLE _programme(
-      annee_univ                  CHAR(9) not null,
-      num_semestre                CHAR(5) not null,
+      annee_univ                  CHAR(9) ,
+      num_semestre                CHAR(5) ,
       id_module                   VARCHAR(7), -- passage en varchar 7
       coefficient                 FLOAT not null,
-      CONSTRAINT PK_PROGRAMME PRIMARY KEY (id_module));
+      CONSTRAINT PK_PROGRAMME PRIMARY KEY (id_module, num_semestre));
       
 
 CREATE TABLE _resultat(
@@ -86,6 +86,24 @@ CREATE TABLE _temp_semestre
 (
       num_semestre                VARCHAR(5),
       annee_univ                  CHAR(9)
+);
+
+CREATE TABLE _candidat_temp(
+      no_candidat         INT,
+      classement          VARCHAR(5),
+      boursier_lycee      VARCHAR(100),--30
+      profil_candidat     VARCHAR(100) not null,--30
+      INE                 VARCHAR(11)PRIMARY KEY,
+      etablissement       VARCHAR(100),--30
+      ville_etablissement VARCHAR(100), --50
+      dept_etablissement  VARCHAR(5), -- 2
+      niveau_etude        VARCHAR(100), --30
+      type_formation_prec VARCHAR(100), --10 de base
+      serie_prec          VARCHAR(100), --2 de base
+      dominante_prec      VARCHAR(100),--30
+      specialite_prec     VARCHAR(100), --50
+      LV1                 VARCHAR(40),--30
+      LV2                 VARCHAR(25)
 );
 
 ALTER TABLE _resultat
@@ -101,12 +119,9 @@ ALTER TABLE _programme
     REFERENCES _module(id_module);
 
 ALTER TABLE _programme
-  ADD CONSTRAINT Fk_programme_smestre_1 FOREIGN KEY (annee_univ)
-    REFERENCES _semestre (annee_univ);
-    
-ALTER TABLE _programme
-  ADD CONSTRAINT Fk_programme_smestre_2 FOREIGN KEY (num_semestre)
-    REFERENCES _semestre (num_semestre);
+  ADD CONSTRAINT Fk_programme_smestre_1 FOREIGN KEY (annee_univ, num_semestre)
+    REFERENCES _semestre (annee_univ, num_semestre);
+   
 
 /*
 ALTER TABLE _inscription
@@ -126,7 +141,28 @@ ALTER TABLE _etudiant
       ADD CONSTRAINT fk_etudiant_incription FOREIGN KEY (id_individu)
           REFERENCES _individu(id_individu);
           
+WbImport -file = data/v_candidatures.csv
+         -header = true
+         -delimiter = ';'
+         -table = _individu
+         -schema = partie2
+         -fileColumns = $wb_skip$, $wb_skip$ , $wb_skip$,  nom, prenom, sexe, date_naissance, nationalite, code_postal, ville, $wb_skip$, $wb_skip$, $wb_skip$, INE;
 
+
+WbImport -file = data2/v_candidatures.csv
+         -header = true
+         -delimiter = ';'
+         -table = _candidat_temp
+         -schema = partie2
+         -filecolumns = $wb_skip$, no_candidat, classement, $wb_skip$, $wb_skip$,$wb_skip$, $wb_skip$, 
+          $wb_skip$, $wb_skip$, $wb_skip$, $wb_skip$, boursier, profil_candidat, INE, $wb_skip$, etablissement,
+          ville_etablissement, dept_etablissement, $wb_skip$, niveau_etude, 
+          type_formation, serie_prec, dominante_prec, specialite_prec, lv1, lv2;       
+
+INSERT INTO _candidat
+    (SELECT id_individu, no_candidat, classement, boursier_lycee, profil_candidat, etablissement, ville_etablissement, niveau_etude, type_formation_prec, serie_prec, dominante_prec, specialite_prec, LV1, LV2
+      FROM _candidat_temp ct
+      INNER JOIN _individu i ON ct.ine = i.ine);
           
 WbImport -file = data2/v_candidatures.csv
          -header = true
@@ -175,10 +211,11 @@ select distinct num_semestre, annee_univ
 from _temp_semestre;
 
 
-WbImport -file= data2/ppn.csv
+WbImport -file= data2/v_programme.csv
          -header = true
          -delimiter = ';'
+         -decimal = ","
          -table = _programme
          -schema = partie2
-         -filecolumns = id_module, $wb_skip$  ,$wb_skip$, coefficient;
+         -filecolumns = annee_univ, num_semestre  ,id_module, coefficient;
  
