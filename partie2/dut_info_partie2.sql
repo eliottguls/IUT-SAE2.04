7bc7e813aa989aea4703fed2f34f5dfcc84ca362
EliottGULS <eliott.guillossou@gmail.com>
2022-05-23 08:52:43 +0200

    candidat + individu injection fixed

--- partie2/dut_info_partie2.sql
+++ partie2/dut_info_partie2.sql
@@ -2,113 +2,137 @@ drop schema if exists partie2 cascade;
 create schema partie2;
 set schema 'partie2';
 
+
+/**********************
+*       CANDIDAT      *
+**********************/
+drop table if exists _candidat;
 CREATE TABLE _candidat(
+      id_individu         INT,
       no_candidat         INT,
-      id_individu         INT,  
-      classement          VARCHAR(5),
-      boursier_lycee      VARCHAR(20) not null, 
-      profil_candidat     VARCHAR(20) not null,
-      etablissement       VARCHAR(40),
-      dept_etablissement  VARCHAR(2),
-      ville_etablissement VARCHAR(30),
-      niveau_etude        VARCHAR(20),
-      type_formation_prec VARCHAR(15),
-      serie_prec          VARCHAR(100), 
-      dominante_prec      VARCHAR(20),
-      specialite_prec     VARCHAR(30),
-      LV1                 VARCHAR(20),
-      LV2                 VARCHAR(25),
-       constraint PK_CANDIDAT primary key (no_candidat)
+      classement          VARCHAR(100),
+      boursier_lycee      VARCHAR(100),--30
+      profil_candidat     VARCHAR(100) not null,--30
+      etablissement       VARCHAR(100),--30
+      dept_etablissement  VARCHAR(100), -- 2
+      ville_etablissement VARCHAR(100), --50
+      niveau_etude        VARCHAR(100), --30
+      type_formation_prec VARCHAR(100), --10 de base
+      serie_prec          VARCHAR(100), --2 de base
+      dominante_prec      VARCHAR(100),--30
+      specialite_prec     VARCHAR(100), --50
+      LV1                 VARCHAR(100),--30
+      LV2                 VARCHAR(100),
+       constraint PK_CANDIDAT primary key (id_individu)
       );
       
+/**********************
+*       INDIVIDU      *
+**********************/
+drop table if exists _individu Cascade;
 CREATE TABLE _individu(
       id_individu         SERIAL,
-      nom                 VARCHAR(30) not null,
-      prenom              VARCHAR(20),
-      date_naissance      DATE not null,
-      code_postal         VARCHAR(30) not null,
-      ville               VARCHAR(50) not null,
-      sexe                VARCHAR(8) not null,
-      nationalite         VARCHAR(20) not null,
-      INE                 VARCHAR(11) not null,
-      CONSTRAINT PK_INDIVIDU PRIMARY KEY(id_individu)
-);
+      nom                 VARCHAR(100),
+      prenom              VARCHAR(100),
+      sexe                VARCHAR(100),
+      date_naissance      DATE ,
+      code_postal         VARCHAR(100), --null
+      ville               VARCHAR(100), --null
+      nationalite         VARCHAR(100),
+      INE                 VARCHAR(100),
+      CONSTRAINT PK_INDIVIDU PRIMARY KEY(id_individu));
     
 
+/**********************
+*       ETUDIANT      *
+**********************/
 CREATE TABLE _etudiant(
-      code_nip            VARCHAR(8), -- = numero etudiant
+      code_nip            VARCHAR(8),
       id_individu         INT,
-      cat_socio_etu       VARCHAR(20),
-      cat_socio_parent    VARCHAR(20),
-      bourse_superieur    BOOLEAN not null,
+      cat_socio_etu       VARCHAR(20) not null,
+      cat_socio_parent    VARCHAR(20) not null,
+      bourse_superieur    BOOLEAN,
       mention_bac         VARCHAR(10),
       serie_bac           VARCHAR(20) not null,
       dominante_bac       VARCHAR(20) not null, 
-      specialite_bac      VARCHAR(20),
+      specialite_bac      VARCHAR(20), 
       mois_annee_obtention_bac    CHAR(7),
       CONSTRAINT PK_ETUDIANT PRIMARY KEY(code_nip));
       
+
+/**********************
+*       SEMESTRE      *
+**********************/
 CREATE TABLE _semestre(
-      id_semestre                 SERIAL, 
+      id_semestre                 INT,
       num_semestre                CHAR(5) not null,
       annee_univ                  CHAR(9) not null,
-      CONSTRAINT PK_SEMESTRE PRIMARY KEY(annee_univ, num_semestre));
+      CONSTRAINT PK_SEMESTRE PRIMARY KEY(id_semestre));
       
+
+/*************************
+*       INSCRIPTION      *
+*************************/
 CREATE TABLE _inscription(
       code_nip                    VARCHAR(8),
-      id_semestre                 SERIAL,
+      id_semestre                 INT,
       groupe_tp                   CHAR(2) not null,
       amenagement_evaluation      VARCHAR(20) not null,
       CONSTRAINT PK_INSCRIPTION PRIMARY kEY(code_nip, id_semestre));
         
+
+/********************
+*       MODULE      *
+********************/
 CREATE TABLE _module(
-      id_module                   VARCHAR(7), -- passage en varchar 7
-      libelle_module              VARCHAR(150) not null, --changement a 150
+      id_module                   VARCHAR(10),
       ue                          CHAR(4) not null,
+      libelle_module              VARCHAR(150) not null,
       CONSTRAINT PK_MODULE PRIMARY KEY (id_module));
       
-
+/***********************
+*      PROGRAMME       *
+***********************/
 CREATE TABLE _programme(
-      annee_univ                  CHAR(9) not null,
-      num_semestre                CHAR(5) not null,
-      id_module                   VARCHAR(7), -- passage en varchar 7
+      id_semestre                 INT,
+      id_module                   CHAR(5),
       coefficient                 FLOAT not null,
-      CONSTRAINT PK_PROGRAMME PRIMARY KEY (id_module));
+      CONSTRAINT PK_PROGRAMME PRIMARY KEY (id_semestre, id_module));
       
-
+/**********************
+*       RESULTAT      *
+**********************/
 CREATE TABLE _resultat(
-      id_module                   VARCHAR(7), -- passage en varchar 7
+      id_module                   CHAR(5),
+      id_semestre                 INT,
       code_nip                    VARCHAR(8),
       moyenne                     FLOAT not null,
-      CONSTRAINT PK_RESULTAT PRIMARY KEY(id_module, code_nip));
-      
-CREATE TABLE _temp_semestre
-(
-      num_semestre                VARCHAR(5),
-      annee_univ                  CHAR(9)
-);
+      CONSTRAINT PK_RESULTAT PRIMARY KEY(id_module, id_semestre, code_nip));
 
+
+/********************
+*    CONTRAINTES    *
+********************/
 ALTER TABLE _resultat
-  ADD CONSTRAINT Fk_resultat_etudiant FOREIGN KEY (code_nip)
+  ADD CONSTRAINT fk_resultat_etudiant FOREIGN KEY (code_nip)
     REFERENCES _etudiant (code_nip);
-
+    
 ALTER TABLE _resultat
-  ADD CONSTRAINT Fk_resultat_module FOREIGN KEY (id_module)
+  ADD CONSTRAINT fk_resultat_module FOREIGN KEY (id_module)
     REFERENCES _module (id_module);
     
-ALTER TABLE _programme
-  ADD CONSTRAINT Fk_programme_module FOREIGN KEY (id_module)
-    REFERENCES _module(id_module);
-
-ALTER TABLE _programme
-  ADD CONSTRAINT Fk_programme_smestre_1 FOREIGN KEY (annee_univ)
-    REFERENCES _semestre (annee_univ);
+ALTER TABLE _resultat
+  ADD CONSTRAINT fk_resultat_semestre FOREIGN KEY (id_semestre)
+    REFERENCES _semestre (id_semestre);
     
 ALTER TABLE _programme
-  ADD CONSTRAINT Fk_programme_smestre_2 FOREIGN KEY (num_semestre)
-    REFERENCES _semestre (num_semestre);
+  ADD CONSTRAINT fk_programme_module FOREIGN KEY (id_module)
+    REFERENCES _module;
+    
+ALTER TABLE _programme
+  ADD CONSTRAINT fk_programme_smestre FOREIGN KEY (id_semestre)
+    REFERENCES _semestre;
 
-/*
 ALTER TABLE _inscription
     ADD CONSTRAINT fk_inscription_semestre FOREIGN KEY (id_semestre)
         REFERENCES _semestre(id_semestre);
@@ -116,8 +140,7 @@ ALTER TABLE _inscription
 ALTER TABLE _inscription
     ADD CONSTRAINT fk_inscription_etudiant FOREIGN KEY (code_nip)
         REFERENCES _etudiant(code_nip);
-*/
-   
+        
 ALTER TABLE _candidat
     ADD CONSTRAINT fk_candidat_postuler_individu FOREIGN KEY (id_individu)
         REFERENCES _individu(id_individu);
@@ -125,77 +148,15 @@ ALTER TABLE _candidat
 ALTER TABLE _etudiant
       ADD CONSTRAINT fk_etudiant_incription FOREIGN KEY (id_individu)
           REFERENCES _individu(id_individu);
-          
-          
-WbImport -file = data/v_candidatures.csv
-         -header = true
-         -delimiter = ';'
-         -table = _individu
-         -schema = partie2
-         -fileColumns = $wb_skip$, $wb_skip$ , $wb_skip$,  nom, prenom, sexe, date_naissance, nationalite, code_postal, ville, $wb_skip$, $wb_skip$, $wb_skip$, INE;
-
-          
-WbImport -file = data2/v_candidatures.csv
-         -header = true
-         -delimiter = ';'
-         -table = _individu
-         -schema = partie2
-         -fileColumns = $wb_skip$, $wb_skip$ , $wb_skip$,  nom, prenom, sexe, date_naissance, nationalite, code_postal, ville, $wb_skip$, $wb_skip$, $wb_skip$, INE;
-
-WbImport -file= data2/ppn.csv
-         -header = true
-         -delimiter = ';'
-         -table = _module
-         -schema = partie2
-         -filecolumns = id_module, ue, libelle_module;
-
-WbImport -file= data2/v_resu_s1.csv
-         -header = true
-         -delimiter = ';'
-         -table = _temp_semestre
-         -schema = partie2
-         -filecolumns = annee_univ, num_semestre;
-;       
-WbImport -file= data2/v_resu_s2.csv
-         -header = true
-         -delimiter = ';'
-         -table = _temp_semestre
-         -schema = partie2
-         -filecolumns = annee_univ, num_semestre;
-         
-WbImport -file= data2/v_resu_s3.csv
-         -header = true
-         -delimiter = ';'
-         -table = _temp_semestre
-         -schema = partie2
-         -filecolumns = annee_univ, num_semestre;
-;        
-WbImport -file= data2/v_resu_s4.csv
-         -header = true
-         -delimiter = ';'
-         -table = _temp_semestre
-         -schema = partie2
-         -filecolumns = annee_univ, num_semestre;
-        
-insert into _semestre (num_semestre, annee_univ)
-select distinct num_semestre, annee_univ
-from _temp_semestre;
 
 
-WbImport -file= data2/ppn.csv
-         -header = true
-         -delimiter = ';'
-         -table = _programme
-         -schema = partie2
-         -filecolumns = id_module, $wb_skip$  ,$wb_skip$, coefficient;
- 
 drop table if exists partie2._candidat_temp;
 CREATE TABLE partie2._candidat_temp(
       no_candidat         INT,
       classement          VARCHAR(5),
       boursier_lycee      VARCHAR(100),--30
       profil_candidat     VARCHAR(100) not null,--30
-      INE                 VARCHAR(11)PRIMARY KEY,
+      INE                 VARCHAR(11),
       etablissement       VARCHAR(100),--30
       ville_etablissement VARCHAR(100), --50
       dept_etablissement  VARCHAR(5), -- 2
@@ -205,11 +166,19 @@ CREATE TABLE partie2._candidat_temp(
       dominante_prec      VARCHAR(100),--30
       specialite_prec     VARCHAR(100), --50
       LV1                 VARCHAR(40),--30
-      LV2                 VARCHAR(25)
+      LV2                 VARCHAR(25),
+      CONSTRAINT PK_CANDIDAT_TEMP PRIMARY KEY(INE)
       );
 
       
 
+WbImport -file = data/v_candidatures.csv
+         -header = true
+         -delimiter = ';'
+         -table = _individu
+         -schema = partie2
+         -fileColumns = $wb_skip$, $wb_skip$ , $wb_skip$,  nom, prenom, sexe, date_naissance, nationalite, code_postal, ville, $wb_skip$, $wb_skip$, $wb_skip$, INE;
+         
 WbImport -file=/home/etuinfo/eguillossou/Documents/IUT_1ST_Y/S2/SAE/SAE2.04/data/v_candidatures.csv
          -header=true
          -delimiter=';'
@@ -224,3 +193,9 @@ INSERT INTO partie2._candidat
     (SELECT id_individu, no_candidat, classement, boursier_lycee, profil_candidat, etablissement, ville_etablissement, niveau_etude, type_formation_prec, serie_prec, dominante_prec, specialite_prec, LV1, LV2
       FROM _candidat_temp ct
       INNER JOIN _individu i ON ct.ine = i.ine);
+
+         
+
+
+
+
